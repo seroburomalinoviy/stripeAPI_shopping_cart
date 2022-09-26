3691bed5a107f4e3959dad9c71eb7194390a8681
Kaminoontyo <mr.ivandmi@yandex.ru>
2022-09-26 14:26:02 +0300

    setting update

--- stripePay/settings.py
+++ stripePay/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+from pathlib import Path
 import environ
 
 env = environ.Env()
 environ.Env.read_env()
 
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
